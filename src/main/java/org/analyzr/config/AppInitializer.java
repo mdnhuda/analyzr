326508efd9595c73de921a7744e8fd9163e9f4e0
Naimul Huda <nhuda@viator.com>
2016-10-04 17:47:04 +1100

    Missing multipart resolver bean added

--- src/main/java/org/analyzr/config/AppInitializer.java
+++ src/main/java/org/analyzr/config/AppInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[] {RootConfig.class};
+        return new Class<?>[] {RootConfig.class, DaoConfig.class};
     }
 
     @Override
--- src/main/java/org/analyzr/config/ServletConfig.java
+++ src/main/java/org/analyzr/config/ServletConfig.java
@@ -1,7 +1,9 @@
 package org.analyzr.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -12,4 +14,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan({"org.analyzr.controller"})
 public class ServletConfig {
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("utf-8");
+        commonsMultipartResolver.setMaxUploadSize(100000000);
+        return commonsMultipartResolver;
+    }
 }
